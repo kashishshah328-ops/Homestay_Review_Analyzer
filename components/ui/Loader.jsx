4fa9e55bd88a46ba0d90f3302cb1f58f2357f6b5
Kashish Shah <kashishshah328@gmail.com>
2026-06-23 07:30:56 +0530

    feat: add reusable UI components

---
+++ components/ui/Loader.jsx
@@ -0,0 +1,19 @@
+/**
+ * Loader Component
+ */
+
+export default function Loader(){
+
+return(
+
+<div className="flex justify-center">
+
+<div className="w-10 h-10 border-4 border-emerald-700 border-t-transparent rounded-full animate-spin">
+
+</div>
+
+</div>
+
+)
+
+}
