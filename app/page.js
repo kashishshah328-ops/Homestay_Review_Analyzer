a40d921dde3e3b496108f1c8ca16c9a1d9bf8ca4
Kashish Shah <kashishshah328@gmail.com>
2026-06-23 12:05:17 +0530

    feat: create home page

--- app/page.js
+++ app/page.js
@@ -8,35 +8,23 @@ export default function Home() {
   const features = [
 
     {
-
-      title:"Sentiment Analysis",
-
-      desc:"Automatically classify reviews as Positive, Neutral or Negative."
-
+      title: "Sentiment Analysis",
+      desc: "Automatically classify reviews as Positive, Neutral or Negative."
     },
 
     {
-
-      title:"Theme Detection",
-
-      desc:"Detect Food, Host, Location, Cleanliness and Experience."
-
+      title: "Theme Detection",
+      desc: "Detect Food, Host, Location, Cleanliness and Experience."
     },
 
     {
-
-      title:"AI Response Generation",
-
-      desc:"Generate personalized management responses."
-
+      title: "AI Response Generation",
+      desc: "Generate personalized management responses."
     },
 
     {
-
-      title:"Review Summarization",
-
-      desc:"Summarize multiple reviews into key insights."
-
+      title: "Review Summarization",
+      desc: "Summarize multiple reviews into key insights."
     }
 
   ];
@@ -47,29 +35,56 @@ export default function Home() {
 
       <Navbar />
 
-      <Hero />
+      <main
+        className="
+        min-h-screen
 
-      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 p-8">
+        bg-gray-50
+        dark:bg-gray-900
 
-        {
+        text-black
+        dark:text-white
+        "
+      >
 
-          features.map((item,index)=>(
+        <Hero />
 
-            <Card
+        <div
+          className="
+          max-w-6xl
+          mx-auto
 
-              key={index}
+          grid
+          grid-cols-1
+          md:grid-cols-2
 
-              title={item.title}
+          gap-6
 
-              desc={item.desc}
+          p-8
+          "
+        >
 
-            />
+          {
 
-          ))
+            features.map((item, index) => (
 
-        }
+              <Card
 
-      </div>
+                key={index}
+
+                title={item.title}
+
+                desc={item.desc}
+
+              />
+
+            ))
+
+          }
+
+        </div>
+
+      </main>
 
       <Footer />
 
