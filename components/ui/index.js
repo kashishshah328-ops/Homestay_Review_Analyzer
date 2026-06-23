77567e3702fae4ff2f01d7e9705a0d68185d959c
Kashish Shah <kashishshah328@gmail.com>
2026-06-23 07:36:59 +0530

    feat: add reusable UI components

---
+++ components/ui/index.js
@@ -0,0 +1,19 @@
+export { default as Button }
+
+from "./Button"
+
+export { default as Input }
+
+from "./Input"
+
+export { default as Modal }
+
+from "./Modal"
+
+export { default as Toast }
+
+from "./Toast"
+
+export { default as Loader }
+
+from "./Loader"
