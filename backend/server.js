690f7acb0902525facde0687dd666361264ed133
Kashish Shah <kashishshah328@gmail.com>
2026-07-11 14:43:19 +0530

    feat: implement JWT authentication, Google OAuth, validation, and protected routes

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,55 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const session = require("express-session");
+require("./config/passport");
+const rateLimit = require("express-rate-limit");
 const reviewRoutes = require("./routes/reviewRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/db");   // <-- ADD THIS
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();                                // <-- ADD THIS
 
 const app = express();
+const authLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
 
+  max: 5,
+
+  message: {
+    message: "Too many login attempts. Please try again after 15 minutes.",
+  },
+
+  standardHeaders: true,
+
+  legacyHeaders: false,
+});
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: "homestay_secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+app.use(passport.initialize());
+
+app.use(passport.session());
 // routes
 app.use("/api/reviews", reviewRoutes);
 
 app.use("/api/dashboard", dashboardRoutes);
+
+app.use("/api/auth", authRoutes);
 // test route
 app.get("/", (req, res) => {
   res.json({
